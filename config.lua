5ffbec590483a81947d9a0d9df5a22bd71688db7
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2021-08-10 02:43:35 +0200

    change coordinates to vector3

--- config.lua
+++ config.lua
@@ -17,32 +17,33 @@ Config.PlateUseSpace = true
 Config.Zones = {
 	
 	ShopEntering = {
-		Pos   = { x = -43.51, y = -1105.47, z = 26.20 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
+		Pos   = vector3(-43.51, -1105.47, 26.20),
+		Size  = vector3(1.5, 1.5, 1.0),
 		Type  = 36
 	},
 	ShopOutside = {
-		Pos   = { x = -29.16, y = -1082.18, z = 26.62 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
-		Heading = 62.11,
+		Pos   = vector3(-29.16, -1082.18, 26.62),
+		Size  = vector3(1.5, 1.5, 1.0),
+		Heading = 338.17,
 		Type  = -1
 	},
 
 	Shop2 = {
-		Pos   = { x = -30.08, y = -1106.94, z = 26.42 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
+		Pos   = vector3(-30.08, -1106.94, 26.42),
+		Size  = vector3(1.5, 1.5, 1.0),
 		Type  = 36
 	},
 	Shop3 = {
-		Pos   = { x = -55.30, y = -1097.39, z = 26.35 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
+		Pos   = vector3(-55.30, -1097.39, 26.35),
+		Size  = vector3(1.5, 1.5, 1.0),
 		Type  = 36
 	},
 
 	ResellVehicle = {
-		Pos   = { x = -46.14, y = -1082.73, z = 25.74 },
-		Size  = { x = 3.0, y = 3.0, z = 1.0 },
+		Pos   = vector3(-46.14, -1082.73, 25.74),
+		Size  = vector3(3.0, 3.0, 1.0),
 		Type  = 1
 	}
 
 }
+
